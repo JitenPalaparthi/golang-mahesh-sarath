a83f882df9ae2785d2aa9bdd507b3ff6c0b8967e
Jiten <jpalaparthi@gmail.com>
2024-09-13 09:37:28 +0530

    add hello world

---
+++ 1-hello/main.go
@@ -0,0 +1,29 @@
+package main
+
+func main() {
+	println("Hello World!")
+	print("Hi folks, lets learn Go")
+}
+
+// main.main
+
+// 1. create a package --> library
+// 2. create a binary/executable
+
+// compile-time vs runtime
+// statically typed programming language
+
+/*
+ - run go application
+ - 1. it is compiled --> compiler
+ - 2. It creates assembly code (plan9 assembly)
+ - 3. assembler -> it concerts the code into object code
+ - 4. linker -> links all required things and creates a binary/executable
+ - 5. the created executable is executed
+
+ - build go application
+ - 1. it is compiled --> compiler
+ - 2. It creates assembly code (plan9 assembly)
+ - 3. assembler -> it concerts the code into object code
+ - 4. linker -> links all required things and creates a binary/executable
+*/
---
+++ go-stufs.md
@@ -0,0 +1,28 @@
+# go programming
+
+## keywords 
+
+package , func 
+
+## builtin functions
+
+print, println 
+
+
+## commands 
+
+- to run go application
+
+```
+go run main.go
+```
+
+- to build
+
+```
+go build main.go
+```
+
+```
+go build -o hello main.go
+```
